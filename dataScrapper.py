d9be35d7a7394dc0bdd8752ff7718cbceb241493
engineerDavid <33636080+engineerDavid@users.noreply.github.com>
2021-04-09 13:02:37 -0500

    updated comments

--- dataScrapper.py
+++ dataScrapper.py
@@ -33,35 +33,34 @@ for r in range(100):
 
 
 
-
     def dataExtraction(CleanQuotes, quotesCounts):
 
         for quoteNum in range(len(CleanQuotes)):
-            #need to start from the second elemnt due to issues with code
-            
-            
             
             string = CleanQuotes[quoteNum]
             
             try:
                 #get rid of some extra text that is not needed
                 quote = string[:string.index("//<![CDATA[")]
+                #get the author from the html tag
                 author = re.search('([^―]*$)', quote)
                 author = author.group(1)
                 author=''.join(author.split())
+                #used to get the quote text betweeen the qutation marks
                 quote = re.search('“(.*?)”', quote)
                 try:
                     quote = quote.group(1)
                 except:
                     print(quote)
                     continue
-               
+               #adds the quotes to dict 
                 my_dict[quotesCounts+quoteNum] = {'Quote': quote, 'Author':author}
                 
                 
                 
                 
             except:
+                #the website has the first 1-3 quotes and authors generted diffrently so this is used to account for the outliers
                 quote = re.search('“(.*?)”', string)
                 author = re.search('([^―]*$)', string)
                 author = author.group(1)
@@ -73,8 +72,10 @@ for r in range(100):
         return my_dict
   
     dataExtraction(CleanQ, quotesCounts)
+    #there are 30 quotes per page and need update the count
     quotesCounts = quotesCounts + 30
 
+#only uncoment when you are ready to save the data to a json file
 # with open('Sample.json', 'w') as fp:
 #     json.dump(my_dict, fp)
 
